0337e3071d22544192ec5ecd17af47ff3f25dc3e
Wang Xinyu <641645560@qq.com>
2023-05-28 15:17:49 +0800

    Add files via upload

---
+++ CSVUtil.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+class CSVUtil:
+
+    def __init__(self, filepath, itemNames):
+        self.filepath = filepath
+        if os.path.exists(filepath):
+            print('CSV文件已存在，无需创建:', filepath)
+            return
+
+        csv_file = open(filepath, 'w', newline='', encoding='gbk')
+        writer = csv.writer(csv_file)
+        writer.writerow(itemNames)
+        csv_file.close()
+
+    def append(self, areaName, areaAll, houseCount, houseArea, otherArea):
+        csv_file = open(self.filepath, 'a', newline='', encoding='gbk')
+        writer = csv.writer(csv_file)
+        writer.writerow([areaName, areaAll, houseCount, houseArea, otherArea])
+        csv_file.close()
+
+    def append(self, data):
+        csv_file = open(self.filepath, 'a', newline='', encoding='gbk')
+        writer = csv.writer(csv_file)
+        writer.writerow(data)
+        csv_file.close()
+
+
---
+++ spider.py
@@ -0,0 +1,104 @@
+import time
+from bs4 import BeautifulSoup
+import bs4
+import re
+import os
+import datetime
+import urllib3
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import requests
+from CSVUtil import CSVUtil
+
+urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+
+
+class ChengDuHouseSpider:
+    def __init__(self):
+        self.url = 'https://www.cdzjryb.com/SCXX/Default.aspx?action=ucEveryday2'
+
+        itemNames = ['区域', '总面积（平方米）', '住宅套数(套)', '住宅面积(平方米)', '非住宅面积（平方米）', '日期']
+        self.bussinessCSV = CSVUtil('./商品房数据.csv', itemNames)
+        self.secondHandCSV = CSVUtil('./二手房数据.csv', itemNames)
+
+    # 下载网页
+    def crawl(self, url):
+        print('正在爬取: ', url)
+        r = ''
+        try:
+            r = requests.get(url, stream=False, headers=headers, verify=False)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            print('requests error')
+        return r
+
+    # 解析网页数据
+    def parse(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        tables = soup.find_all('table', {"class": "blank"}, recursive=True)
+        if not tables and len(tables)==0:
+            print('table is empty')
+            return
+        if len(tables) != 2:
+            print('table is incomplete')
+            return
+
+        j = 0
+        for tr in tables[0].children:
+            if type(tr) is bs4.element.NavigableString:
+                continue
+
+            j = j + 1
+            print(j, '---', tr)
+
+
+        today = datetime.datetime.now().strftime('%Y%m%d')
+
+        # 商品房今日成交
+        i = 0
+        for tr in tables[0].children:
+            if type(tr) is bs4.element.NavigableString:
+                continue
+            i = i + 1
+            if i < 3:
+                continue
+            bussinessData = []
+            for td in tr.children:
+                if type(td) is bs4.element.NavigableString:
+                    continue
+                bussinessData.append(td.text.strip())
+
+            bussinessData.append(today)
+            self.bussinessCSV.append(bussinessData)
+            print(bussinessData)
+
+        i = 0
+        # 二手房今日成交
+        for tr in tables[1].children:
+            if type(tr) is bs4.element.NavigableString:
+                continue
+            i = i + 1
+            if i < 3:
+                continue
+            secondHandData = []
+            for td in tr.children:
+                if type(td) is bs4.element.NavigableString:
+                    continue
+                secondHandData.append(td.text.strip())
+
+            secondHandData.append(today)
+            self.secondHandCSV.append(secondHandData)
+            print(secondHandData)
+
+    def start(self):
+        html = self.crawl(self.url)
+        self.parse(html.content)
+        print('%s日数据爬取完成'%datetime.datetime.now().strftime('%Y%m%d'))
+
+if __name__ == "__main__":
+    spider = ChengDuHouseSpider()
+    spider.start()
