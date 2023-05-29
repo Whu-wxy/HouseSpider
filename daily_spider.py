b0c8872f75833687697cdb49ddecceff1c4b5c85
Wang Xinyu <641645560@qq.com>
2023-05-29 21:31:41 +0800

    Update daily_spider.py

--- daily_spider.py
+++ daily_spider.py
@@ -1,11 +1,16 @@
 import schedule
 from ChengDuHouseSpider import ChengDuHouseSpider
+import time
 
 def job():
     spider = ChengDuHouseSpider()
     spider.start()
 
-schedule.every().day.at("15:33").do(job) # 每天某时间执行
+schedule.every().day.at("22:00").do(job) # 每天某时间执行
 
+print("start daily_spider")
 while True:
+    time.sleep(1)
     schedule.run_pending()
+
+print("daily_spider stop")
